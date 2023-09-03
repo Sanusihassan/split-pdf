48cc579e6d3ad98b9b47628cce982e345329d4b1
Sanusi Hassan <sanusihassan122@gmail.com>
2023-09-03 03:13:55 +0000

    added dnd functionality to custom ranges

--- components/Options/SplitPDF/SplitByRange/CustomRange.tsx
+++ components/Options/SplitPDF/SplitByRange/CustomRange.tsx
@@ -4,49 +4,25 @@ import { XIcon, PlusIcon } from "@heroicons/react/solid";
 import { useCallback, useEffect, useState } from "react";
 import { TypeWithdisplayProp } from "./FixedRange";
 import { Checkbox } from "pretty-checkbox-react";
+import { DragDropContext, Draggable, DropResult } from "react-beautiful-dnd";
 
-import { useDrag, useDrop } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { DndProvider } from "react-dnd";
-const ItemTypes = {
-  RANGE: 'range',
+import { Droppable } from "react-beautiful-dnd";
+type Ranges = {
+  from: number;
+  to: number;
+}[];
+
+const reorder = (
+  list: Ranges,
+  startIndex: number,
+  endIndex: number
+): Ranges => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
 };
-
-// Inside the mapping function
-// const [{ isDragging }, drag] = useDrag<
-//   { type: string; index: number },
-//   unknown,
-//   { isDragging: boolean }
-// >({
-//   item: { type: ItemTypes.RANGE, index: number },
-//   collect: (monitor: DragSourceMonitor) => ({
-//     isDragging: monitor.isDragging(),
-//   }),
-// });
-
-// const [, drop] = useDrop<
-//   { type: string; index: number },
-//   unknown,
-//   { isOver: boolean }
-// >({
-//   accept: ItemTypes.RANGE,
-//   hover: (item, monitor) => {
-//     const draggingIndex = item.index;
-//     const targetIndex = i;
-
-//     // Reorder the ranges array
-//     if (draggingIndex !== targetIndex) {
-//       setRanges((prevRanges) => {
-//         const updatedRanges = [...prevRanges];
-//         const draggingRange = updatedRanges[draggingIndex];
-//         updatedRanges.splice(draggingIndex, 1);
-//         updatedRanges.splice(targetIndex, 0, draggingRange);
-//         return updatedRanges;
-//       });
-//     }
-//   },
-// });
-
 
 export const CustomRange = ({ display }: TypeWithdisplayProp) => {
   const [from, setFrom] = useState("");
@@ -70,32 +46,49 @@ export const CustomRange = ({ display }: TypeWithdisplayProp) => {
   const [ranges, setRanges] = useState<{ from: number; to: number }[]>([
     { from: 1, to: pageCount as number },
   ]);
+
+  const onDragEnd = (result: DropResult) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const updatedRanges = reorder(
+      ranges,
+      result.source.index,
+      result.destination.index
+    );
+
+    setRanges(updatedRanges);
+  };
   const handleAddRangeClick = () => {
     if (from && to) {
       setRanges([...ranges, { from: parseInt(from), to: parseInt(to) }]);
       setFrom("");
       setTo("");
-    } else {
-      if (ranges.length > 0) {
-        const lastRange = ranges[ranges.length - 1];
-        if (lastRange.to == pageCount) {
-          setRanges([...ranges, { from: pageCount, to: pageCount }]);
-        } else {
-          setRanges([
-            ...ranges,
-            {
-              from:
-                lastRange.from + 1 < pageCount
-                  ? lastRange.from + 1
-                  : lastRange.from,
-              to: pageCount,
-            },
-          ]);
-        }
-      } else {
-        setRanges([...ranges, { from: 1, to: pageCount }]);
-      }
     }
+    // bug to fix:
+    // else {
+    //   if (ranges.length > 0) {
+    //     const lastRange = ranges[ranges.length - 1];
+    //     if (lastRange.to == pageCount) {
+    //       setRanges([...ranges, { from: pageCount, to: pageCount }]);
+    //     } else {
+    //       setRanges([
+    //         ...ranges,
+    //         {
+    //           from:
+    //             lastRange.from + 1 < pageCount
+    //               ? lastRange.from + 1
+    //               : lastRange.from,
+    //           to: pageCount,
+    //         },
+    //       ]);
+    //     }
+    //   } else {
+    //     setRanges([...ranges, { from: 1, to: pageCount }]);
+    //   }
+    // }
   };
 
   const handleDeleteRangeClick = (index: number) => {
@@ -124,72 +117,105 @@ export const CustomRange = ({ display }: TypeWithdisplayProp) => {
           />
         </div>
       </div>
-      {/* consider these ranges, i want to make each range draggable and it should be only dragged and dropped on the y axis i.e vertically if you like. */}
-      {ranges.map((range, i) => (
-        <DndProvider backend={HTML5Backend}>
-          <div className="row mb-3 range" key={i}>
-            <div className="col">
-              <div className="card">
-                <div className="card-body">
-                  <h6 className="ignore-margin card-title split-category-title">
-                    Range {i}
-                  </h6>
-                  <div className="row flex-nowrap justify-content-between align-items-center range-input-wrapper">
-                    <div className="row flex-nowrap input-group">
-                      <span className="input-group-text" id="basic-addon1">
-                        From
-                      </span>
-                      <input
-                        type="number"
-                        className="form-control"
-                        value={range.from}
-                        onChange={(e) =>
-                          setRanges(
-                            ranges.map((r) =>
-                              r.to === range.to
-                                ? { ...r, from: parseInt(e.target.value, 10) }
-                                : r
-                            )
-                          )
-                        }
-                        placeholder="From"
-                      />
-                    </div>
-                    <div className="row flex-nowrap input-group">
-                      <span className="input-group-text" id="basic-addon1">
-                        To
-                      </span>
-                      <input
-                        type="number"
-                        className="form-control"
-                        value={range.to}
-                        onChange={(e) =>
-                          setRanges(
-                            ranges.map((r) =>
-                              r.to === range.to
-                                ? { ...r, to: parseInt(e.target.value, 10) }
-                                : r
-                            )
-                          )
-                        }
-                        placeholder="To"
-                      />
-                    </div>
-                    {/* <div className="position-absolute delete-button"> */}
-                    <button
-                      className="btn btn-link text-danger"
-                      onClick={() => handleDeleteRangeClick(i)}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="ranges" direction="horizontal">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {ranges.map((range, i) => (
+                <Draggable key={i} draggableId={i.toString()} index={i}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
                     >
-                      <XIcon className="icon" />
-                    </button>
-                    {/* </div> */}
-                  </div>
-                </div>
-              </div>
+                      <div className="row mb-3 range">
+                        <div className="col">
+                          <div className="card">
+                            <div className="card-body">
+                              <h6 className="ignore-margin card-title split-category-title">
+                                Range {i}
+                              </h6>
+                              <div className="row flex-nowrap justify-content-between align-items-center range-input-wrapper">
+                                <div className="row flex-nowrap input-group">
+                                  <span
+                                    className="input-group-text"
+                                    id="basic-addon1"
+                                  >
+                                    From
+                                  </span>
+                                  <input
+                                    type="number"
+                                    className="form-control"
+                                    value={range.from}
+                                    onChange={(e) =>
+                                      setRanges(
+                                        ranges.map((r) =>
+                                          r.to === range.to
+                                            ? {
+                                                ...r,
+                                                from: parseInt(
+                                                  e.target.value,
+                                                  10
+                                                ),
+                                              }
+                                            : r
+                                        )
+                                      )
+                                    }
+                                    placeholder="From"
+                                  />
+                                </div>
+                                <div className="row flex-nowrap input-group">
+                                  <span
+                                    className="input-group-text"
+                                    id="basic-addon1"
+                                  >
+                                    To
+                                  </span>
+                                  <input
+                                    type="number"
+                                    className="form-control"
+                                    value={range.to}
+                                    onChange={(e) =>
+                                      setRanges(
+                                        ranges.map((r) =>
+                                          r.to === range.to
+                                            ? {
+                                                ...r,
+                                                to: parseInt(
+                                                  e.target.value,
+                                                  10
+                                                ),
+                                              }
+                                            : r
+                                        )
+                                      )
+                                    }
+                                    placeholder="To"
+                                  />
+                                </div>
+                                {/* <div className="position-absolute delete-button"> */}
+                                <button
+                                  className="btn btn-link text-danger"
+                                  onClick={() => handleDeleteRangeClick(i)}
+                                >
+                                  <XIcon className="icon" />
+                                </button>
+                                {/* </div> */}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
             </div>
-          </div>
-        </DndProvider>
-      ))}
+          )}
+        </Droppable>
+      </DragDropContext>
       <div className="row justify-content-center add-btn">
         <button
           className="col-6 btn btn-dark row align-items-center"
